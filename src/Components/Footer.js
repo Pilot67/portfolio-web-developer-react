f5c356b353cc55fe4a6704af2b1735dde4f943aa
Stuart <86697483+Pilot67@users.noreply.github.com>
2022-01-17 18:27:43 +1100

    fixed footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,42 +3,46 @@ import "./css/footer.css";
 
 function Footer() {
   return (
-    <footer>
-      <section className="container">
-      <div>
-        <h4>Melbourne, Australia</h4>
-      </div>
-      <div>
-        <a href={"https://github.com/Pilot67"} target="_blank" rel="noreferrer">
-          <img
-            className="footerImage"
-            src={require("./images/github-white.png")}
-            alt="github"
-          />
-        </a>
-        <a
-          href={"https://www.linkedin.com/in/stuart-simmons-7a818325/"}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="footerImage"
-            src={require("./images/linkedin-white.png")}
-            alt="LinkedIn"
-          />
-        </a>
-        <a
-          href={"https://stackoverflow.com/users/16347112/stuart"}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img
-            className="footerImage"
-            src={require("./images/stack-overflow-white.png")}
-            alt="StackOverflow"
-          />
-        </a>
-      </div>
+    <footer className="footer">
+      <section className="footerContainer">
+          <div>
+            <h4>Melbourne, Australia</h4>
+          </div>
+          <div>
+            <a
+              href={"https://github.com/Pilot67"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="footerImage"
+                src={require("./images/github-white.png")}
+                alt="github"
+              />
+            </a>
+            <a
+              href={"https://www.linkedin.com/in/stuart-simmons-7a818325/"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="footerImage"
+                src={require("./images/linkedin-white.png")}
+                alt="LinkedIn"
+              />
+            </a>
+            <a
+              href={"https://stackoverflow.com/users/16347112/stuart"}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                className="footerImage"
+                src={require("./images/stack-overflow-white.png")}
+                alt="StackOverflow"
+              />
+            </a>
+          </div>
       </section>
     </footer>
   );
