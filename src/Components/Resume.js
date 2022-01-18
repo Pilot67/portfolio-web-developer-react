4235f757113ea51378441946d6588d00aabaa3d2
Stuart <86697483+Pilot67@users.noreply.github.com>
2022-01-18 11:29:37 +1100

    added resume items

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,12 +1,44 @@
 import React from "react";
+import "./css/resume.css"
 
 function Resume() {
   return (
     <div className="container">
       <section className="cardBodyWide">
-      <div className="cardText">
-        <h1>Hi World</h1>
-      </div>
+        <div className="cardText cardTextAbout">
+          <h3>Resume</h3>
+          <br/>
+          <h5>Software development competencies</h5>
+          <br/>
+          <ul className="competenciesList">
+            <li >Frontend</li>
+            <ul className="competenciesItems">
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>Javascript</li>
+              <li>JQuery</li>
+              <li>Bootstrap</li>
+              <li>Responsive Design - Mobile First</li>
+              <li>React</li>
+              <li>Progressive Web Applications</li>
+            </ul>
+            <br/>
+            <li>Backend</li>
+            <ul className="competenciesItems">
+              <li>API's</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>SQL</li>
+              <li>NoSQL Database</li>
+              <li>MySQL, Sequelize</li>
+              <li>Mongoose, MongoDB</li>
+              <li>GraphQL</li>
+            </ul>
+          </ul>
+          <ul>
+            <li></li>
+          </ul>
+        </div>
       </section>
     </div>
   );
